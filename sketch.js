c2f0a5645ecdfa76042a91fa747d49f8bd306777
Bia <82560900+BiatrizR@users.noreply.github.com>
2022-06-28 07:33:58 -0300

    Update sketch.js
    add animations.

--- sketch.js
+++ sketch.js
@@ -9,13 +9,25 @@ var ground
 var planodefundo
 var anigador
 var jogador
-var fundao
+var stars, starsImg, starsGroup
 var inimigoNormal
 var inimigoRapido
 var inimigoReforcado
+var angle = 90;
+
+var enemyOne1, enemyTwo2, laser1
+
 function preload() {
-anigador = loadAnimation("./assets/SpaceShip/s1.png","./assets/SpaceShip/s2.png","./assets/SpaceShip/s3.png",)
-planodefundo = loadImage("./assets/bgSpace.gif")
+  anigador = loadAnimation("./assets/SpaceShip/s1.png","./assets/SpaceShip/s2.png","./assets/SpaceShip/s3.png");
+ 
+
+  starsImg = loadImage("./assets/stars.jpg");
+
+  enemyOne1 = loadAnimation(  "./assets/enemys/enemy5.png", "./assets/enemys/enemy6.png",  "./assets/enemys/enemy7.png");
+  enemyTwo2 = loadAnimation(  "./assets/enemys/enemy1.png", "./assets/enemys/enemy2.png",  "./assets/enemys/enemy3.png");
+  laser1 = loadAnimation("./assets/PNG/Explosion_7/2/Explosion_1_1.png", "./assets/PNG/Explosion_7/2/Explosion_1_2.png", "./assets/PNG/Explosion_7/2/Explosion_1_3.png")
+ 
+  
 }
 
 
@@ -27,25 +39,30 @@ function setup() {
   canvas = createCanvas(800, 600);
   engine = Engine.create();
   world = engine.world;
+  starsGroup = new Group();
 
-  fundao = createSprite(width/2,height/2,1200,600)
-  fundao.addImage("imagemdofundao",planodefundo)
-  fundao.velocityY=3
+   
+ 
 
-  jogador = createSprite(width/2,height/2+200,50,50)
-  jogador.addAnimation("animaçãojogador",anigador)
+  jogador = createSprite(width/2,height/2+200,50,50);
+  jogador.addAnimation("animaçãojogador",anigador);
   jogador.frameDelay=10
   
 
 }
 
 function draw(){  
+
+  background("black")
   Engine.update(engine);
   drawSprites();
 
-  if(fundao.y>400){
-   fundao.y=fundao.height/2;
-  }
+
+  /*if(stars.y > 600){
+   stars.y=stars.height/8;
+  } */
+
+
   if(keyDown(LEFT_ARROW)){
     jogador.x=jogador.x-4
   }
@@ -53,16 +70,59 @@ function draw(){
     jogador.x=jogador.x+4
   }
 
+  var ran = Math.round(random(1, 2));
+
+ if( frameCount % 30 == 0 ){
+  if(ran == 1){
+    enemyOne(velocityX = 2)
+  
+  } else if(ran == 2){
+    enemyOne(velocityX = -2)
+  }}
+  angleMode(DEGREES);
+
+
+  /*for (var j = 0; j < totalUV; j++) {
+    if (grupoRaiosUV.get(j) != undefined && grupoRaiosUV.get(j).collide(ozonio)) {
+      grupoRaiosUV.get(j).destroy();
+      grupoRaiosUV.destroyEach();
+    }
+  }*/
 }
 
-function enemyOne(){
+function enemyOne(velocity){
+  rotate(angle)
+  var enemy1 = createSprite(600, 40);
+
+  enemy1.addAnimation("ENEMYONE", enemyOne1);
+ //enemyOne1.frameDelay = 5
+  enemy1.scale = 0.13;
+  enemy1.velocityX = velocity;
+  
+
+  enemy1.x = Math.round(random(0, 800));
+
+
+  var laser = createSprite(enemy1.x, enemy1.y+50);
+  laser.addAnimation("laser", laser1);
+  laser.scale = 0.2;
+  laser.velocityY= +5
 
 
 }
-function enemyTwo(){
+
+function stars(){
+  stars = createSprite(400, 300);
+  stars.addImage("imagemdostars", starsImg);
+  stars.scale = 0.15;  
+  starsGroup.add(stars);
 
 
 }
-function enemythree(){
+function tirosCurtos(){
+
+
+}
+function tirosLongos(){
 
 }
